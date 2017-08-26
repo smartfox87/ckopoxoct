3837e4277d0cda291ddfc62090146387be0d1c50
Andrei Dyminski <smoke59@mail.ru>
2017-08-26 12:49:19 +0300

    Update 2017-08-26T09:49:06.307Z

--- js/zcript.js
+++ js/zcript.js
@@ -1,5 +1,7 @@
 $(function () {
   var menu = $(".main-nav__list");
+  var fixedMenuDesktop = $(".main-nav").stickme();
+
   $(".promo").owlCarousel({
     items: 1,
     nav: true,
@@ -7,6 +9,7 @@ $(function () {
     dots: false,
     loop: true
   });
+
   if ($(window).outerWidth() < 768) {
     $(".rates__list").owlCarousel({
       items: 1,
@@ -16,6 +19,7 @@ $(function () {
       loop: true
     });
   }
+
   $(".main-nav__toggle-menu").click(function (event) {
     event.preventDefault();
     if ($(this).hasClass("main-nav__toggle-menu--open")) {
@@ -28,5 +32,52 @@ $(function () {
       menu.removeClass("main-nav__list--show");
     }
   });
-  var fixedMenuDesktop = $(".main-nav").stickme();
+
+  function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+  function writeCostMonth(output) {
+    $(output).data("cost", $(output).text());
+  }
+
+  function calcDiscount(input, output) {
+    var costMonth = $(output).data("cost");
+    var countMonth = $(input).val();
+    var totalCost = "";
+    switch (countMonth) {
+      case "1":
+        totalCost = costMonth;
+        break;
+      case "3":
+        totalCost = (countMonth * costMonth) - (countMonth * costMonth) / 100 * parseInt($("#discount--3").text());
+        break;
+      case "6":
+        totalCost = (countMonth * costMonth) - (countMonth * costMonth) / 100 * parseInt($("#discount--6").text());
+        break;
+      case "12":
+        totalCost = (countMonth * costMonth) - (countMonth * costMonth) / 100 * parseInt($("#discount--12").text());
+    }
+    if (isNumeric(totalCost)) {
+      return totalCost;
+    }
+    return "Некорректное число";
+  }
+
+  function outputCostRate(call, input, output) {
+    var cost = call(input, output);
+    $(output).text(cost);
+  }
+
+  writeCostMonth("#rates__output--4");
+  writeCostMonth("#rates__output--2");
+
+  $("#rates__4").change(function () {
+    outputCostRate(calcDiscount, "#rates__4", "#rates__output--4");
+  });
+
+  $("#rates__2").change(function () {
+    outputCostRate(calcDiscount, "#rates__2", "#rates__output--2");
+  });
+
 });
